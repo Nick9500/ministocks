484450809760c8eeec2e82a5b01f58d2acdf6fc5
GurkomalSRao <gsrao@hotmail.ca>
2018-02-12 00:31:44 -0500

    updated Attempts

--- src/main/java/nitezh/ministock/activities/widget/Bonobo_widget_provider.java
+++ src/main/java/nitezh/ministock/activities/widget/Bonobo_widget_provider.java
@@ -1,17 +1,48 @@
 package nitezh.ministock.activities.widget;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.RemoteViews;
-import nitezh.ministock.R;
 
-public class Bonobo_widget_provider extends AppWidgetProvider{
+import nitezh.ministock.CustomAlarmManager;
+import nitezh.ministock.R;
+import nitezh.ministock.WidgetProvider;
+
+public class Bonobo_widget_provider extends WidgetProviderBase{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
+        String action = intent.getAction();
+
+        if (action != null) {
+            switch (action) {
+                case "LEFT":
+                case "RIGHT":
+                    Bundle extras = intent.getExtras();
+                    if (extras != null) {
+                        int appWidgetId = extras.getInt(
+                                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                                AppWidgetManager.INVALID_APPWIDGET_ID);
+                        handleTouch(context, appWidgetId, action);
+                    }
+                    break;
+
+                default:
+                    super.onReceive(context, intent);
+                    break;
+            }
+        }
+    }
+
+    private void handleTouch(Context context, int appWidgetId, String action) {
+        if (action.equals("LEFT")) {
+            startPreferencesActivity(context, appWidgetId);
+        }
     }
 
     @Override
@@ -20,8 +51,10 @@ public class Bonobo_widget_provider extends AppWidgetProvider{
         for (int widgetId : appWidgetIds) {
             RemoteViews mView = initViews(context, appWidgetManager, widgetId);
             appWidgetManager.updateAppWidget(widgetId, mView);
+            //
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
     }
     private RemoteViews initViews(Context context,
                                   AppWidgetManager widgetManager, int widgetId) {
@@ -33,8 +66,18 @@ public class Bonobo_widget_provider extends AppWidgetProvider{
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
 
         intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-        mView.setRemoteAdapter(widgetId, R.id.widgetCollectionList, intent);
+        mView.setRemoteAdapter( R.id.widgetCollectionList, intent);
+
+        Intent leftTouchIntent = new Intent(context, WidgetProvider.class);
+        leftTouchIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+        leftTouchIntent.setAction("LEFT");
+        mView.setOnClickPendingIntent(R.id.widget_left,
+                PendingIntent.getBroadcast(context, widgetId, leftTouchIntent, 0));
+
 
         return mView;
     }
+
+
+
 }

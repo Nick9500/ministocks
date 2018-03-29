fe1e103b6fabda7e2e9a8afae912d21086b0f904
CristiArde <c_burns93@yahoo.ca>
2018-03-29 14:25:40 -0400

    Adding test for ImportFeature. mock JavaInputStream/BufferdReader #39 #38

--- src/test/java/nitezh/ministock/domain/ImportFeatureTest.java
+++ src/test/java/nitezh/ministock/domain/ImportFeatureTest.java
@@ -4,11 +4,17 @@ import android.os.Environment;
 
 import com.thoughtworks.xstream.io.path.Path;
 
+import org.hamcrest.core.IsEqual;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
 import org.robolectric.RuntimeEnvironment;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,10 +23,11 @@ import nitezh.ministock.activities.GlobalWidgetData;
 import nitezh.ministock.activities.PreferencesActivity;
 import nitezh.ministock.activities.widget.WidgetRow;
 
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.mockito.Mockito.*;
+
 
 /**
  * Created by Cristi Arde on 3/22/2018.
@@ -49,12 +56,24 @@ public class ImportFeatureTest {
         File file = new File("src/test/java/nitezh/ministock/domain/stocks.csv");
         assertTrue(file.exists());
     }
+    @Test
+    public void mockBufferReader() throws IOException {
+        BufferedReader bufferedReader = mock(BufferedReader.class);
+        String line1 = "AAPL";
+        String line2 = "GOOGL";
+        when(bufferedReader.readLine()).thenReturn(line1).thenReturn(line2);
+        assertEquals(line1, bufferedReader.readLine());
+        assertEquals(line2, bufferedReader.readLine());
+    }
+
 
     @Test
     public void csvToString()
     {
         File file = getFileFromPath("stocks.csv");
         assertThat(file, notNullValue());
+
+
     }
 
 

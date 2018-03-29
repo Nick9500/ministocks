ba108385bebcfa6934ad5bca104f023f8e4999b3
CristiArde <c_burns93@yahoo.ca>
2018-03-29 15:23:26 -0400

    Adding test for ImportFeature. tested getting live quote data from given symbol tested reading buffer for each line symbol #39 #38

--- src/test/java/nitezh/ministock/domain/ImportFeatureTest.java
+++ src/test/java/nitezh/ministock/domain/ImportFeatureTest.java
@@ -1,31 +1,31 @@
 package nitezh.ministock.domain;
 
-import android.os.Environment;
-
-import com.thoughtworks.xstream.io.path.Path;
-
-import org.hamcrest.core.IsEqual;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.internal.util.reflection.Whitebox;
-import org.robolectric.RuntimeEnvironment;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ArrayList;
 
-import nitezh.ministock.activities.GlobalWidgetData;
-import nitezh.ministock.activities.PreferencesActivity;
-import nitezh.ministock.activities.widget.WidgetRow;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+
+import nitezh.ministock.mocks.MockCache;
+import nitezh.ministock.mocks.MockStorage;
+import nitezh.ministock.mocks.MockWidgetRepository;
 
 import static junit.framework.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 
@@ -38,12 +38,20 @@ import static org.mockito.Mockito.*;
 
 public class ImportFeatureTest {
 
-   /* @Before
+    private StockQuoteRepository stockRepository;
+
+    @Before
     public void setUp() {
 
+         MockWidgetRepository mockWidgetRepository = new MockWidgetRepository();
+         mockWidgetRepository.setWidgetsStockSymbols(new HashSet<>(Arrays.asList(
+                 "AAPL",
+                 "TSLA"
+                )));
+         stockRepository = new StockQuoteRepository(
+                 new MockStorage(), new MockCache(), mockWidgetRepository);
 
-
-    }*/
+    }
    private static File getFileFromPath(String fileName) {
 
        return new File("src/test/java/nitezh/ministock/domain/"+ fileName);
@@ -68,13 +76,53 @@ public class ImportFeatureTest {
 
 
     @Test
-    public void csvToString()
-    {
+    public void csvToString() throws IOException {
+
         File file = getFileFromPath("stocks.csv");
         assertThat(file, notNullValue());
 
+        String testSteam = "AAPL\nGOOGL\nTESL";
+        InputStream stream = new ByteArrayInputStream(testSteam.getBytes(StandardCharsets.UTF_8));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
+        assertEquals("AAPL", reader.readLine());
+        assertEquals("GOOGL", reader.readLine());
+        assertEquals("TESL", reader.readLine());
     }
 
+    @Test
+    public void getQuotefromSymbol()
+    {
+        List<String> symbols = new ArrayList<String>();
+        symbols.add("AAPL");
+        symbols.add("TSLA");
+
+        //getLiveQuotes
+        HashMap<String, StockQuote> quotes = stockRepository.getLiveQuotes(symbols);
+
+        //check if return anything
+        System.out.println(quotes.size());
+        assertEquals(2, quotes.size());
+
+        //check if returned real data
+        StockQuote aaplQuote = quotes.get("AAPL");
+        assertEquals("AAPL", aaplQuote.getSymbol());
+        assertThat(aaplQuote.getSymbol(), notNullValue());
+        assertThat(aaplQuote.getPrice(), notNullValue());
+        assertThat(aaplQuote.getPercent(), notNullValue());
+        System.out.println("Symbol: "+ aaplQuote.getSymbol());
+        System.out.println("Price: "+ aaplQuote.getPrice());
+        System.out.println("Percent: "+ aaplQuote.getPercent());
+
+        StockQuote teslaQuote = quotes.get("TSLA");
+        assertEquals("TSLA", teslaQuote.getSymbol());
+        assertThat(teslaQuote.getSymbol(), notNullValue());
+        assertThat(teslaQuote.getPrice(), notNullValue());
+        assertThat(teslaQuote.getPercent(), notNullValue());
+        System.out.println("Symbol: "+ teslaQuote.getSymbol());
+        System.out.println("Price: "+ teslaQuote.getPrice());
+        System.out.println("Percent: "+ teslaQuote.getPercent());
+
+    }
 
 }

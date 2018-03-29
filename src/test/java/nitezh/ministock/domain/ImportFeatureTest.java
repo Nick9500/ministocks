684bcda8577f5c20eac32a308e478a4cdd820561
CristiArde <c_burns93@yahoo.ca>
2018-03-29 13:51:55 -0400

    Adding test for ImportFeature. Check if filexists, check content #39 #38

--- src/test/java/nitezh/ministock/domain/ImportFeatureTest.java
+++ src/test/java/nitezh/ministock/domain/ImportFeatureTest.java
@@ -2,12 +2,15 @@ package nitezh.ministock.domain;
 
 import android.os.Environment;
 
+import com.thoughtworks.xstream.io.path.Path;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.RuntimeEnvironment;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import nitezh.ministock.activities.GlobalWidgetData;
@@ -15,6 +18,8 @@ import nitezh.ministock.activities.PreferencesActivity;
 import nitezh.ministock.activities.widget.WidgetRow;
 
 import static junit.framework.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.mockito.Mockito.*;
 
 /**
@@ -26,19 +31,31 @@ import static org.mockito.Mockito.*;
 
 public class ImportFeatureTest {
 
-    @Before
+   /* @Before
     public void setUp() {
 
 
 
-    }
+    }*/
+   private static File getFileFromPath(String fileName) {
+
+       return new File("src/test/java/nitezh/ministock/domain/"+ fileName);
+   }
 
     @Test
     public void testIfFileExists()
     {
-        File file = mock(File.class);
-        when(file.exists()).thenReturn(true);
+        //using project path since SD card directory cannot be accessible during tests
+        File file = new File("src/test/java/nitezh/ministock/domain/stocks.csv");
         assertTrue(file.exists());
     }
+
+    @Test
+    public void csvToString()
+    {
+        File file = getFileFromPath("stocks.csv");
+        assertThat(file, notNullValue());
+    }
+
 
 }

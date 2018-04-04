1f7632ded255e2eb38753e9f7dd0eb2108156755
GurkomalSRao <gsrao@hotmail.ca>
2018-04-04 17:51:53 -0400

    #57  AlphaVantage Tests Complete
    every test case complete.
    validates data retrieval for all time intervals (Daily, Weekly, Monthly)
    Values can be stored as json objects

--- src/main/java/nitezh/ministock/dataaccess/AlphaVantageRepository.java
+++ src/main/java/nitezh/ministock/dataaccess/AlphaVantageRepository.java
@@ -9,6 +9,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import nitezh.ministock.domain.StockQuote;
 import nitezh.ministock.utils.Cache;
@@ -21,7 +23,8 @@ import nitezh.ministock.utils.UrlDataTools;
 public class AlphaVantageRepository {
 
     private static final String API_KEY = "ZKD8M6L9CEQAK89H";
-    private String baseURL = "https://www.alphavantage.co/query?function=";
+    private String baseURL = "https://www.alphavantage.co/query?function=TIME_SERIES_";
+    private String cryptoBaseURL = "https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_";
     private final FxChangeRepository fxChangeRepository;
 
     AlphaVantageRepository(FxChangeRepository fxChangeRepository) {
@@ -59,14 +62,15 @@ public class AlphaVantageRepository {
         return quotes;
     }*/
 
+    //for regular stock exchange
     JSONArray retrieveQuotesAsJson(Cache cache, String timeInterval, String symbols) throws JSONException {
         String url = buildRequestURL(timeInterval, symbols);
         String quotesString = UrlDataTools.getCachedUrlData(url, cache, 300);
         JSONObject quotesJson = new JSONObject(quotesString);
-
         JSONArray quotes = new JSONArray();
-        quotesJson = quotesJson.getJSONObject("Time Series (Daily)");
-        for (Iterator key = quotesJson.keys(); key.hasNext();) /*TODO iterate through dates properly*/
+        Iterator<String> stringKey = quotesJson.keys();
+        quotesJson = quotesJson.getJSONObject(stringKey.next());
+        for (Iterator key = quotesJson.keys(); key.hasNext();)
         {
             String objKey = (String)key.next();
             JSONObject quoteJson =  quotesJson.getJSONObject(objKey);
@@ -76,14 +80,45 @@ public class AlphaVantageRepository {
             data.put("high", quoteJson.optString("2. high"));
             data.put("low", quoteJson.optString("3. low"));
             data.put("close", quoteJson.optString("4. close"));
-            data.put("volume", quoteJson.optString("5. latestVolume"));
+            data.put("volume", quoteJson.optString("5. volume"));
             quotes.put(data);
        }
 
         return quotes;
     }
 
+
+    //For Crypto Currency
+    JSONArray retrieveQuotesAsJson(Cache cache, String timeInterval, String symbols, String market) throws JSONException {
+        String url = buildRequestURL(timeInterval, symbols, market);
+        String quotesString = UrlDataTools.getCachedUrlData(url, cache, 300);
+        JSONObject quotesJson = new JSONObject(quotesString);
+        JSONArray quotes = new JSONArray();
+        Iterator<String> stringKey = quotesJson.keys();
+        quotesJson = quotesJson.getJSONObject(stringKey.next());
+        for (Iterator key = quotesJson.keys(); key.hasNext();)
+        {
+            String objKey = (String)key.next();
+            JSONObject quoteJson =  quotesJson.getJSONObject(objKey);
+            JSONObject data = new JSONObject();
+            data.put("date", objKey);
+            data.put("open", quoteJson.optString("1a. open ("+market+")"));
+            data.put("high", quoteJson.optString("2a. high ("+market+")"));
+            data.put("low", quoteJson.optString("3a. low ("+market+")"));
+            data.put("close", quoteJson.optString("4a. close ("+market+")"));
+            data.put("volume", quoteJson.optString("5. volume"));
+            data.put("market cap", quoteJson.optString("6. market cap (USD)")); //market cap always in USD
+            quotes.put(data);
+        }
+
+        return quotes;
+    }
+
     private String buildRequestURL(String timeInterval, String symbol){
         return baseURL+timeInterval+"&symbol="+symbol+"&interval=15min&outputsize=full&apikey="+API_KEY;
+    }
+
+    private String buildRequestURL(String timeInterval, String symbol, String market){
+        return cryptoBaseURL+timeInterval+"&symbol="+symbol+"&market="+market+"&apikey="+API_KEY;
     }
 }
--- src/test/java/nitezh/ministock/dataaccess/AlphaVantageRepositoryTests.java
+++ src/test/java/nitezh/ministock/dataaccess/AlphaVantageRepositoryTests.java
@@ -2,11 +2,19 @@ package nitezh.ministock.dataaccess;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nitezh.ministock.mocks.MockCache;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -16,29 +24,67 @@ import static org.junit.Assert.assertNotNull;
 public class AlphaVantageRepositoryTests {
     private AlphaVantageRepository alphaVantageRepository;
     JSONArray vantageList;
-    String symbols;
-    String timeInterval;
+    List<String> timeInterval;
+    String symbol;
+
     @Before
     public void setUp() {
-    vantageList = null;
-    FxChangeRepository fxRepository = new FxChangeRepository();
-        this.alphaVantageRepository = new AlphaVantageRepository(fxRepository);
+        timeInterval = new ArrayList<>();
+        timeInterval.add("DAILY");
+        timeInterval.add("WEEKLY");
+        timeInterval.add("MONTHLY");
+        vantageList = null;
+        FxChangeRepository fxRepository = new FxChangeRepository();
+            this.alphaVantageRepository = new AlphaVantageRepository(fxRepository);
 }
 
     @Test
-    public void retrieveDailyValues() throws JSONException{
-        symbols = "GOOG";
-        timeInterval = "TIME_SERIES_DAILY";
-        vantageList = returnJSONArray(timeInterval,symbols);
-        assertNotNull(vantageList);
-        Assert.assertEquals(1005, vantageList.length());
-        /*JSONObject jsonObject = vantageList.optJSONObject(0);                    <---- returns unordered dates after it was retrieved
-        //Assert.assertEquals("1092.7400", jsonObject.optString("open"));
-        //Assert.assertEquals("1106.3000", jsonObject.optString("high"));*/
+    public void retrieveRegularStockValues() throws JSONException{
+        symbol = "GOOG";
+        //test data retrieval for all time intervals
+        for(String timeInterval : this.timeInterval) {
+            vantageList = returnJSONArray(timeInterval, symbol);
+            assertNotNull(vantageList);
+            assertNotEquals(0, vantageList.length());
+            JSONObject jsonObject = vantageList.optJSONObject(0);                    //validate if all fields of a specific date are properly received
+
+            assertFalse(jsonObject.optString("date").isEmpty());
+            assertFalse(jsonObject.optString("open").isEmpty());
+            assertFalse(jsonObject.optString("high").isEmpty());
+            assertFalse(jsonObject.optString("low").isEmpty());
+            assertFalse(jsonObject.optString("close").isEmpty());
+            assertFalse(jsonObject.optString("volume").isEmpty());
+        }
     }
+
+    @Test
+    public void retrieveDailyCryptoCurrencyValues() throws JSONException{
+        symbol = "BTC";
+        String market = "USD";
+        //test data retrieval for all time intervals
+        for(String timeInterval : this.timeInterval) {
+            vantageList = returnCryptoJSONArray(timeInterval, symbol, market);
+            assertNotNull(vantageList);
+            assertNotEquals(0, vantageList.length());
+            JSONObject jsonObject = vantageList.optJSONObject(0);                    //validate if all fields of a specific date are properly received
+            assertFalse(jsonObject.optString("date").isEmpty());
+            assertFalse(jsonObject.optString("open").isEmpty());
+            assertFalse(jsonObject.optString("high").isEmpty());
+            assertFalse(jsonObject.optString("low").isEmpty());
+            assertFalse(jsonObject.optString("close").isEmpty());
+            assertFalse(jsonObject.optString("volume").isEmpty());
+            assertFalse(jsonObject.optString("market cap").isEmpty());
+        }
+    }
+
+
 
     private JSONArray returnJSONArray(String timeInterval, String symbols) throws JSONException{
         return alphaVantageRepository.retrieveQuotesAsJson(new MockCache(), timeInterval, symbols);
+    }
+
+    private JSONArray returnCryptoJSONArray(String timeInterval, String symbols, String market) throws JSONException{
+        return alphaVantageRepository.retrieveQuotesAsJson(new MockCache(), timeInterval, symbols, market);
     }
 
 }

2b2c788ccddefd02c6d624f838b5774934d43621
Raj Rajashansi <raj34@live.ca>
2018-04-09 23:25:41 -0400

    Get All Data Test
    Testing if all data is obtained when no stock ID is provided. Issues: #66 , #39.

--- src/test/java/nitezh/ministock/dataaccess/CoinMarketCapRepoTest.java
+++ src/test/java/nitezh/ministock/dataaccess/CoinMarketCapRepoTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import nitezh.ministock.mocks.MockCache;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertNotEquals;
@@ -19,11 +20,24 @@ public class CoinMarketCapRepoTest {
     CoinMarketCapRepo coinMarketCapRepo;
 
     @Test
-    public void getAPIData() throws JSONException{
-        String id = "bitcoin";
+    public void getAllAPIData() throws JSONException{
+        String blankID = "";  //should get all the data if no id is provided
+        int NumberOfTotalStocks = 100;  //Number of total stocks from CoinMarketRepo
+
+        coinMarketCapRepo = new CoinMarketCapRepo(new FxChangeRepository());
+        JSONArray jsonArray = coinMarketCapRepo.retrieveQuotesAsJson(blankID);
+        assertNotNull(jsonArray);
+        assertEquals(NumberOfTotalStocks , jsonArray.length());  //we should be getting every stock
+    }
+    @Test
+    public void getAPIDataByID() throws JSONException{
+        String id = "bitcoin"; //will get the data only related to bitcoin
         coinMarketCapRepo = new CoinMarketCapRepo(new FxChangeRepository());
         JSONArray jsonArray = coinMarketCapRepo.retrieveQuotesAsJson(id);
         assertNotNull(jsonArray);
+        assertEquals(1, jsonArray.length());  //we should only be getting bitcoin
     }
+
+
 
 }
